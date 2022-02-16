2820b31c8e668474529df7e90fa9ab2a88ba0175
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-16 18:15:10 +0800

    yaml加载

--- dev.py
+++ dev.py
@@ -1,6 +1,7 @@
 #!/opt/homebrew/bin/python3
 
 import time
+import yaml
 from apscheduler.schedulers.background import BackgroundScheduler
 
 scheduler = BackgroundScheduler(timezone = 'UTC')
@@ -10,6 +11,8 @@ def hello():
 
 scheduler.add_job(hello, 'cron', second = 58)
 scheduler.start()
+
+print(yaml.safe_load(open('.secret.yaml', 'r')))
 
 while True:
   time.sleep(1)
