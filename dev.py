805eed28887558a3ece550761b16fd84352c1a7d
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-16 18:05:48 +0800

    .secret.yaml

--- dev.py
+++ dev.py
@@ -8,7 +8,7 @@ scheduler = BackgroundScheduler(timezone = 'UTC')
 def hello():
   print('你好，世界')
 
-scheduler.add_job(hello, 'interval', seconds = 1)
+scheduler.add_job(hello, 'cron', second = 58)
 scheduler.start()
 
 while True:
