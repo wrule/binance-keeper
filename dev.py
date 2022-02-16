1b1ee4f716b4a581e2b4d73edbc51adcbc32d458
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-16 17:38:01 +0800

    建立项目

--- dev.py
+++ dev.py
@@ -1,3 +1,15 @@
 #!/opt/homebrew/bin/python3
 
-print('你好，世界')
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+
+scheduler = BackgroundScheduler(timezone = 'UTC')
+
+def hello():
+  print('你好，世界')
+
+scheduler.add_job(hello, 'interval', seconds = 1)
+scheduler.start()
+
+while True:
+  time.sleep(1)
