e133c3336b9fbbc82c8df04f6048b39c2fbde94c
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-17 15:30:01 +0800

    不用垃圾库

--- dev.py
+++ dev.py
@@ -1,17 +1,10 @@
 #!/opt/homebrew/bin/python3
-
-import time
 import yaml
-from apscheduler.schedulers.background import BackgroundScheduler
-from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+
+secret = yaml.safe_load(open('.secret.yaml', 'r'))
 
 def main():
-  symbol = 'BTCUSDT'
-  secret = yaml.safe_load(open('.secret.yaml', 'r'))
   print('你好，世界')
-  client = Client(secret['APIKEY'], secret['APISECRET'])
-  time_res = client.get_server_time()
-  print(time_res)
 
 if __name__ == '__main__':
   main()
