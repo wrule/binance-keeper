7e8e1672574073e9b2cd505bcd92c3df3aef61e1
jimao <jimao@jimaodeMBP.lan>
2022-02-17 21:36:22 +0800

    提交代码

--- dev.py
+++ dev.py
@@ -1,22 +1,31 @@
 #!/opt/homebrew/bin/python3
 
 import time
-from binance.websocket.spot.websocket_client import SpotWebsocketClient as Client
+from binance.websocket.spot.websocket_client import SpotWebsocketClient as WSClient
+from binance.spot import Spot as Client
+from datetime import datetime
 
-def message_handler(data):
-  if 'k' in data.keys():
-    print(data['k'])
+spot_client = Client('https://testnet.binance.vision')
+klines = spot_client.klines('BTCUSDT', '1d', limit = 100)
+for row in klines:
+  print(datetime.fromtimestamp(row[0] / 1e3), row[4])
+print(len(klines))
 
-my_client = Client('wss://testnet.binance.vision')
-my_client.start()
 
-my_client.kline(
-  symbol = 'btcusdt',
-  id = 1,
-  interval = '1m',
-  callback = message_handler,
-)
+# def message_handler(data):
+#   if 'k' in data.keys():
+#     print(data['k'])
 
-time.sleep(3000)
+# my_client = WSClient('wss://testnet.binance.vision')
+# my_client.start()
 
-my_client.stop()
+# my_client.kline(
+#   symbol = 'btcusdt',
+#   id = 1,
+#   interval = '1m',
+#   callback = message_handler,
+# )
+
+# time.sleep(3000)
+
+# my_client.stop()
