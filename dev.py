703b77b5ade5c27731df6096d357e9255d6f0348
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-17 15:39:11 +0800

    获取账户信息

--- dev.py
+++ dev.py
@@ -1,10 +1,18 @@
 #!/opt/homebrew/bin/python3
 import yaml
+import requests
+from binance.spot import Spot
 
 secret = yaml.safe_load(open('.secret.yaml', 'r'))
 
 def main():
   print('你好，世界')
+  # r = requests.get('https://www.baidu.com')
+  # print(r.status_code)
+  client = Spot(secret['APIKEY'], secret['APISECRET'])
+  # Get account information
+  print(client.account())
+
 
 if __name__ == '__main__':
   main()
