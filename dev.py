8d6ce8fe918a302cd285b86e59f4bd1498b8b5e8
jimao <jimao@jimaodeMBP.lan>
2022-02-17 22:02:16 +0800

    k线类

--- dev.py
+++ dev.py
@@ -5,10 +5,27 @@ from binance.websocket.spot.websocket_client import SpotWebsocketClient as WSCli
 from binance.spot import Spot as Client
 from datetime import datetime
 
+class K:
+  def __init__(self, data):
+    self.time = data[0] / 1e3
+    self.open = float(data[1])
+    self.high = float(data[2])
+    self.low = float(data[3])
+    self.close = float(data[4])
+    self.volume = float(data[5])
+  time = 0
+  open = 0
+  close = 0
+  high = 0
+  low = 0
+  volume = 0
+
+
 spot_client = Client('https://testnet.binance.vision')
-klines = spot_client.klines('BTCUSDT', '1d', limit = 100)
+klines = spot_client.klines('BTCUSDT', '1d', limit = 10)
 for row in klines:
-  print(datetime.fromtimestamp(row[0] / 1e3), row[4])
+  k = K(row)
+  print(datetime.fromtimestamp(k.time), k.close)
 print(len(klines))
 
 
@@ -29,3 +46,6 @@ print(len(klines))
 # time.sleep(3000)
 
 # my_client.stop()
+
+
+
