f16605781038feb8a4ac477698a4c7952d18b834
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-17 15:10:39 +0800

    一般的http接口

--- dev.py
+++ dev.py
@@ -9,6 +9,9 @@ def main():
   symbol = 'BTCUSDT'
   secret = yaml.safe_load(open('.secret.yaml', 'r'))
   print('你好，世界')
+  client = Client(secret['APIKEY'], secret['APISECRET'])
+  time_res = client.get_server_time()
+  print(time_res)
 
 if __name__ == '__main__':
   main()
