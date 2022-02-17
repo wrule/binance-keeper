6412804d18fcd4c27cba74dfa7010d223dcb5819
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-17 18:08:39 +0800

    kline

--- dev.py
+++ dev.py
@@ -4,16 +4,23 @@ import time
 from binance.websocket.spot.websocket_client import SpotWebsocketClient as Client
 
 def message_handler(data):
-  if 'c' in data.keys():
-    print(data['c'])
+  if 'k' in data.keys():
+    print(data['k'])
 
-my_client = Client()
+my_client = Client('wss://testnet.binance.vision')
 my_client.start()
 
-my_client.mini_ticker(
-    symbol="btcusdt",
-    id=2,
-    callback=message_handler,
+# my_client.mini_ticker(
+#     symbol="btcusdt",
+#     id=2,
+#     callback=message_handler,
+# )
+
+my_client.kline(
+  symbol = 'btcusdt',
+  id = 1,
+  interval = '1m',
+  callback = message_handler,
 )
 
 time.sleep(3000)
